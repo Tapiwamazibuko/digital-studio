d42ba4fbfad709642855d0874dc7068a6fac206e
DS-HEADOFFICE\tapiwa.mazibuko <tapiwa.mazibuko@dariel.co.za>
2024-01-22 10:54:47 +0200

    Adding loading screen

---
+++ src/components/LoadingScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function LoadingScreen(){
+    return (     
+        <div className="loading--container">
+            <h1 className="loading--text">LOADING</h1>
+        </div>  
+    )
+}
