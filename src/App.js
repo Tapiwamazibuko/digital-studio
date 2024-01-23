455fa2d2941376ea07f0f2a77176d7fea87da150
DS-HEADOFFICE\tapiwa.mazibuko <tapiwa.mazibuko@dariel.co.za>
2024-01-23 08:45:54 +0200

    adding Index

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import LoadingScreen from './components/LoadingScreen';
 import EntryScreen from './components/EntryScreen';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Index from './components/Index';
+import React from 'react';
 
 function App() {
+  const [isLoading, setIsLoading] = React.useState(true)
+  const stillLoading = async () => {
+    setTimeout(() => {  setIsLoading(false); }, 3000);
+  }
+
+  stillLoading()
+
   return (
+    isLoading ? <LoadingScreen /> :
     <div>
       <Header />
-      <EntryScreen />
+      <Index />
       <Footer />
-    </div>    
+    </div>   
   );
 }
 
---
+++ src/components/Index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Index(){
+    return (
+        <div className="index--background">
+            <div className="entry--button--container">
+                <button type="button" className="index--button">View our work</button>
+            </div>
+        </div>
+    )
+}
