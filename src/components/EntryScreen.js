c2d26ca0af454c745bd297f20eb6969a30f5bf80
DS-HEADOFFICE\tapiwa.mazibuko <tapiwa.mazibuko@dariel.co.za>
2024-01-29 12:11:48 +0200

    enter without audio

--- src/components/EntryScreen.js
+++ src/components/EntryScreen.js
@@ -1,15 +1,16 @@
 import React from "react";
 
 export default function EntryScreen(props){
-    const [style, setStyle] = React.useState("")
+    const [style, setStyle] = React.useState({})
+    const [lineStyle, setLineStyle] = React.useState({})
     function buttonHover(){
-        console.log("sonething!")
         setStyle(() =>  {
             return {
                 color: "white",
                 backgroundColor: "rgb(50,50,60)",
                 transform: "translate(-1px, -1px)",
-                transition: "transform 0.3s, opacity 0.3s"
+                transition: "transform 0.3s, opacity 0.3s",
+                cursor: "pointer"
             }
             
         })
@@ -25,6 +26,27 @@ export default function EntryScreen(props){
             
         })
     }
+
+    function lineHover(){
+        setLineStyle(() => {
+            return { 
+                
+                transform: "scaleX(0.1) translate(-10px, 0px)",
+                transition: "transform .2s ease-out",
+                cursor: "pointer"
+            }
+        })
+    }
+
+    function lineLeave(){
+        setLineStyle(() => {
+            return { 
+                width: "112px"
+            }
+        })
+    }
+
+
     return (
         <main className="entry--background">
             <div className="entry--button--container">
@@ -32,8 +54,13 @@ export default function EntryScreen(props){
                 onClick={props.entry} onMouseOver={buttonHover} onMouseLeave={buttonLeave}
                 >Enter</button>
             </div>
-            <div className="entry--container">
-                <h4 className="entry--text">ENTER WITHOUT AUDIO</h4>
+            <div className="entry--container" style={{cursor: "pointer"}} onMouseOver={lineHover} onMouseLeave={lineLeave}>
+                <h4 className="entry--text"  >
+                    ENTER WITHOUT AUDIO
+                    <span className="left--span" style={lineStyle} 
+                    ></span>
+                    <span className="right--span"></span>
+                </h4>
             </div>
         </main>  
     )
